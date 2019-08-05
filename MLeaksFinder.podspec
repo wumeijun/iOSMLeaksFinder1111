49a3cb9eaeb80b14b716430944ad9418d63839db
Maggie <wumeijun@soulapp.cn>
2019-08-05 11:29:35 +0800

    把内存泄漏的数据上传到后端去

--- MLeaksFinder.podspec
+++ MLeaksFinder.podspec
@@ -21,11 +21,11 @@ Pod::Spec.new do |s|
 #TODO: Add long description of the pod here.
 #                       DESC
 
-  s.homepage         = "https://github.com/Zepo/MLeaksFinder"
+  s.homepage         = "https://github.com/wumeijun/iOSMLeaksFinder"
   # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
   s.license          = 'MIT'
-  s.author           = { "Zeposhe" => "zeposhe@163.com" }
-  s.source           = { :git => "https://github.com/Zepo/MLeaksFinder.git", :tag => s.version }
+  s.author           = { "wumeijun" => "1852151879@163.com" }
+  s.source           = { :git => "https://github.com/wumeijun/iOSMLeaksFinder.git", :tag => s.version }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '6.0'
--- MLeaksFinder/MLeaksMessenger.m
+++ MLeaksFinder/MLeaksMessenger.m
@@ -1,14 +1,10 @@
-/**
- * Tencent is pleased to support the open source community by making MLeaksFinder available.
- *
- * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- *
- * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *
- * https://opensource.org/licenses/BSD-3-Clause
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
+//
+//  MLeaksMessenger.m
+//  MLeaksFinder
+//
+//  Created by 佘泽坡 on 7/17/16.
+//  Copyright © 2016 zeposhe. All rights reserved.
+//
 
 #import "MLeaksMessenger.h"
 
@@ -16,10 +12,11 @@ static __weak UIAlertView *alertView;
 
 @implementation MLeaksMessenger
 
+static NSString * leakSavePath =  @"http://qm.soulapp-inc.cn/api/vulpix/vulpix_view/post_check_point_data/";
+
 + (void)alertWithTitle:(NSString *)title message:(NSString *)message {
     [self alertWithTitle:title message:message delegate:nil additionalButtonTitle:nil];
 }
-
 
 + (void)alertWithTitle:(NSString *)title
                message:(NSString *)message
@@ -32,8 +29,8 @@ static __weak UIAlertView *alertView;
                                                   cancelButtonTitle:@"OK"
                                                   otherButtonTitles:additionalButtonTitle, nil];
     
-    //    [alertViewTemp show];
-    //    alertView = alertViewTemp;
+//    [alertViewTemp show];
+//    alertView = alertViewTemp;
     NSString *className = [self getClassName:message];
     
     NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
@@ -41,6 +38,11 @@ static __weak UIAlertView *alertView;
     NSString *userPhoneName = [[UIDevice currentDevice] name];
     NSString *viewStack = [message stringByReplacingOccurrencesOfString:@"\n" withString:@""];
     NSLog(@"%@: %@", title, className);
+    
+    NSDictionary *params = @{@"class_name":className, @"app_version":appVersion,
+                             @"view_stack":message, @"phone_model":userPhoneName};
+    [self sendLeakResult:params];
+
 }
 
 /**
@@ -56,5 +58,77 @@ static __weak UIAlertView *alertView;
     NSArray *className = [temp componentsSeparatedByString:@","];
     return [className objectAtIndex:0];
 }
+
+/**
+ *  发送内存泄漏数据到测试平台
+ **/
++ (void)sendLeakResult:(NSDictionary *)dicData{
+    
+    NSString *boundary = @"wumeijun2019";
+    NSURL *url = [NSURL URLWithString:leakSavePath];
+    NSMutableString *bodyContent = [NSMutableString string];
+    for(NSString *key in dicData.allKeys){
+        id value = [dicData objectForKey:key];
+        [bodyContent appendFormat:@"--%@\r\n",boundary];
+        [bodyContent appendFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n",key];
+        [bodyContent appendFormat:@"%@\r\n",value];
+    }
+    [bodyContent appendFormat:@"--%@--\r\n",boundary];
+    NSData *bodyData=[bodyContent dataUsingEncoding:NSUTF8StringEncoding];
+    NSMutableURLRequest *request  = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:10];
+    [request addValue:[NSString stringWithFormat:@"multipart/form-data;boundary=%@",boundary] forHTTPHeaderField:@"Content-Type"];
+    [request addValue: [NSString stringWithFormat:@"%zd",bodyData.length] forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:bodyData];
+    NSLog(@"请求的长度%@",[NSString stringWithFormat:@"%zd",bodyData.length]);
+    __autoreleasing NSError *error=nil;
+    __autoreleasing NSURLResponse *response=nil;
+    NSLog(@"输出Bdoy中的内容>>\n%@",[[NSString alloc]initWithData:bodyData encoding:NSUTF8StringEncoding]);
+    NSData *reciveData= [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if(error){
+        NSLog(@"出现异常%@",error);
+    }else{
+        NSHTTPURLResponse *httpResponse=(NSHTTPURLResponse *)response;
+        if(httpResponse.statusCode==200){
+            NSLog(@"服务器成功响应!>>%@",[[NSString alloc]initWithData:reciveData encoding:NSUTF8StringEncoding]);
+            
+        }else{
+            NSLog(@"服务器返回失败>>%@",[[NSString alloc]initWithData:reciveData encoding:NSUTF8StringEncoding]);
+            
+        }
+    }
+        
+
+
+//    NSLog(@"发送请求url=%@,params=%@",leakSavePath,params);
+//    NSDictionary *headers = @{ @"Content-Type": @"application/x-www-form-urlencode"};
+//    NSData *postData = [NSJSONSerialization dataWithJSONObject:params options:0 error:nil];
+//
+//
+////    NSArray * result = [NSJSONSerialization JSONObjectWithData:postData options:NSJSONReadingMutableLeaves error:nil];
+//
+//
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:leakSavePath]
+//                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
+//                                                       timeoutInterval:10.0];
+//    [request setHTTPMethod:@"POST"];
+//    [request setAllHTTPHeaderFields:headers];
+//    [request setHTTPBody:postData];
+//
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request
+//                                                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//                                                    if (error) {
+//                                                        NSLog(@"leakerror%@", error);
+//                                                    } else {
+//                                                        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+//                                                        NSLog(@"leakresponse%@", httpResponse);
+//                                                    }
+//                                                }];
+//    [dataTask resume];
+    
+}
+
+
 
 @end
